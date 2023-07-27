4129f2ddb77f7472601a37d069763781ac38fe63
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-07-27 17:30:03 -0400

    Added logic for share button

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+const shareBtnContainer = document.querySelector(
+  ".author-container__share-btn"
+);
+const authorContainer = document.querySelector(".author-container");
+const shareContainer = document.querySelector(".share-container");
+const hideBtnContainer = document.querySelector(".share-container__hide-btn");
+
+shareBtnContainer.addEventListener("click", function (e) {
+  const shareBtn = e.target.closest(".author-container__share-btn");
+  if (!shareBtn) return;
+  authorContainer.classList.toggle("hidden");
+  shareContainer.classList.toggle("hidden");
+});
+
+hideBtnContainer.addEventListener("click", function (e) {
+  const hideBtn = e.target.closest(".share-container__hide-btn");
+  if (!hideBtn) return;
+  authorContainer.classList.toggle("hidden");
+  shareContainer.classList.toggle("hidden");
+});
