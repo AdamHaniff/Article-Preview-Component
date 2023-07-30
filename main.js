52044787b6f0bc1b13b6f8980113455f540c449c
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-07-29 22:14:22 -0400

    Created desktop share button

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const viewportWidth = window.innerWidth;
 const shareBtnContainer = document.querySelector(
   ".author-container__share-btn"
 );
+const shareBtnContainerDesktop = document.querySelector(
+  ".author-container-desktop__share-btn"
+);
 const authorContainer = document.querySelector(".author-container");
 const shareContainer = document.querySelector(".share-container");
 const hideBtnContainer = document.querySelector(".share-container__hide-btn");
@@ -16,34 +19,34 @@ const hideBtnContainerDesktop = document.querySelector(
 );
 let shareBtn;
 
-if (viewportWidth <= 1439) {
-  shareBtnContainer.addEventListener("click", function (e) {
-    const shareBtn = e.target.closest(".author-container__share-btn");
-    if (!shareBtn) return;
-    authorContainer.classList.toggle("hidden");
-    shareContainer.classList.toggle("hidden");
-  });
+shareBtnContainer.addEventListener("click", function (e) {
+  const shareBtn = e.target.closest(".author-container__share-btn");
+  if (!shareBtn) return;
+  authorContainer.classList.toggle("hidden");
+  shareContainer.classList.toggle("hidden");
+});
 
-  hideBtnContainer.addEventListener("click", function (e) {
-    const hideBtn = e.target.closest(".share-container__hide-btn");
-    if (!hideBtn) return;
-    authorContainer.classList.toggle("hidden");
-    shareContainer.classList.toggle("hidden");
-  });
-} else {
-  shareBtnContainer.addEventListener("click", function (e) {
-    shareBtn = e.target.closest(".author-container__share-btn");
-    if (!shareBtn) return;
-    shareBtn.classList.toggle("hidden");
-    shareContainerDesktop.classList.toggle("hidden");
-    hideBtnContainerDesktop.classList.toggle("hidden");
-  });
+hideBtnContainer.addEventListener("click", function (e) {
+  const hideBtn = e.target.closest(".share-container__hide-btn");
+  if (!hideBtn) return;
+  authorContainer.classList.toggle("hidden");
+  shareContainer.classList.toggle("hidden");
+});
 
-  hideBtnContainerDesktop.addEventListener("click", function (e) {
-    const hideBtn = e.target.closest(".share-container-desktop__hide-btn");
-    if (!hideBtn) return;
-    hideBtnContainerDesktop.classList.toggle("hidden");
-    shareBtn.classList.toggle("hidden");
-    shareContainerDesktop.classList.toggle("hidden");
-  });
-}
+// DESKTOP
+
+shareBtnContainerDesktop.addEventListener("click", function (e) {
+  shareBtn = e.target.closest(".author-container-desktop__share-btn");
+  if (!shareBtn) return;
+  shareBtn.classList.toggle("hidden");
+  shareContainerDesktop.classList.toggle("hidden");
+  hideBtnContainerDesktop.classList.toggle("hidden");
+});
+
+hideBtnContainerDesktop.addEventListener("click", function (e) {
+  const hideBtn = e.target.closest(".share-container-desktop__hide-btn");
+  if (!hideBtn) return;
+  hideBtnContainerDesktop.classList.toggle("hidden");
+  shareBtn.classList.toggle("hidden");
+  shareContainerDesktop.classList.toggle("hidden");
+});
